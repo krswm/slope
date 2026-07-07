32812ce12fb98726c22bc1f4613fd9d8f4a01de1
Kurosawa Mutsumi <248625291+krswm@users.noreply.github.com>
2026-07-08 08:01:09 +0900

    feat(st-to-tf): parse size of header of Safetensors file

--- safetensors_to_tenferro/src/main.rs
+++ safetensors_to_tenferro/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// Specification:
+// https://github.com/safetensors/safetensors
+
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let safetensors_path = "../../gpt2/model.safetensors";
+    let mut file = std::fs::File::open(safetensors_path)?;
+
+    let mut size_of_header_buffer = [0; 8];
+    file.read_exact(&mut size_of_header_buffer)?;
+    let size_of_header = u64::from_le_bytes(size_of_header_buffer);
+    println!("size_of_header: {size_of_header}");
+
+    Ok(())
 }
