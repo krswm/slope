2a79613fa51e2c8b35c00680133acb7f454eaee6
Kurosawa Mutsumi <248625291+krswm@users.noreply.github.com>
2026-07-08 08:14:10 +0900

    feat(st-to-tf): load header of Safetensors file

--- safetensors_to_tenferro/src/main.rs
+++ safetensors_to_tenferro/src/main.rs
@@ -7,10 +7,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let safetensors_path = "../../gpt2/model.safetensors";
     let mut file = std::fs::File::open(safetensors_path)?;
 
-    let mut size_of_header_buffer = [0; 8];
+    let mut size_of_header_buffer = [0u8; 8];
     file.read_exact(&mut size_of_header_buffer)?;
-    let size_of_header = u64::from_le_bytes(size_of_header_buffer);
+    let size_of_header = usize::from_le_bytes(size_of_header_buffer);
     println!("size_of_header: {size_of_header}");
+
+    let mut header_buffer = vec!(0u8; size_of_header);
+    file.read_exact(&mut header_buffer)?;
+    let header = str::from_utf8(&header_buffer)?;
+    println!("header: {header}");
 
     Ok(())
 }
