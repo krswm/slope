e4b7f34fd174cc0390a3ee4d3eea5dc74ec7eca5
Kurosawa Mutsumi <248625291+krswm@users.noreply.github.com>
2026-07-14 12:09:05 +0900

    feat(tokenizer): load merges.txt

--- src/bin/tokenizer.rs
+++ src/bin/tokenizer.rs
@@ -8,7 +8,7 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
@@ -20,7 +20,33 @@ fn main() -> Result<(), Box<dyn Error>> {
         serde_json::from_reader(reader)?
     };
 
-    println!("{token_to_id:?}");
+    let ranks = {
+        let path = &format!("{}/merges.txt", &args[1]);
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        let lines = reader.lines();
+
+        let mut ranks: HashMap<(usize, usize), u32> = HashMap::new();
+        let mut rank = 0;
+        for line in lines.map_while(Result::ok) {
+            // Skip a comment line.
+            if line.starts_with("#") {
+                continue;
+            }
+
+            let mut split = line.split(" ");
+            let token0 = split.next().unwrap();
+            let token1 = split.next().unwrap();
+            let id0 = &token_to_id[token0];
+            let id1 = &token_to_id[token1];
+
+            ranks.insert((*id0, *id1), rank);
+            rank += 1;
+        }
+        ranks
+    };
+
+    println!("{ranks:?}");
 
     Ok(())
 }
